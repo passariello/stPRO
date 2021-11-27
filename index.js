eefc0d53b1ba18cfd92b10d6ab912b0389bfcda2
Dario Passariello <dariopassariello@gmail.com>
2021-11-26 22:41:27 -0800

    first setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * stPRO <https://github.com/passariello/stPRO>
+ *
+ * Copyright (c) 2021, Dario Passariello.
+ * Licensed under the Apache-2.0 License.
+ */
+
+"use strict";
+
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+};
